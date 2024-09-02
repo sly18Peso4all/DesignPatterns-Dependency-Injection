6c1791c7f426f619d940be8995cf731ba1002e2f
Joseph Agwu Onuoha <joseph.AgwuOnuoha@lichtblick.de>
2024-09-02 15:07:26 +0200

    Updated Players Choice

--- DesignPatterns-Dependency-Injection.git/HumanPlayer.cs
+++ DesignPatterns-Dependency-Injection.git/HumanPlayer.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DesignPatterns_Dependency_Injection.git
 {
     public class HumanPlayer : IPlayer
@@ -16,23 +11,28 @@ namespace DesignPatterns_Dependency_Injection.git
             {
                 Console.WriteLine("Enter Choice: Rock (R), Paper (P) or Scissors (S) : ");
 
-                string input = Console.ReadLine()!.ToUpper();
+                string input = Console.ReadLine().ToUpper();
 
-                switch (input)
+                if (input == "R")
                 {
-                    case "R":
-                        playerChoice = Choice.Rock;
-                        break;
-                    case "P":
-                        playerChoice = Choice.Paper;
-                        break;
-                    case "S":
-                        playerChoice = Choice.Scissors;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid choice, do try again");
-                        break;
-                };
+                    playerChoice = Choice.Rock;
+                    break;
+                }
+                else if (input == "P")
+                {
+                    playerChoice = Choice.Paper;
+                    break;
+                }
+                else if (input == "S")
+                {
+                    playerChoice = Choice.Scissors;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice, try again");
+                }
+
             } while (true);
 
             return playerChoice;
--- DesignPatterns-Dependency-Injection.git/Program.cs
+++ DesignPatterns-Dependency-Injection.git/Program.cs
@@ -1,4 +1,23 @@
 ﻿
 using DesignPatterns_Dependency_Injection.git;
 
-GameManager gameManager = new GameManager()
+
+GameManager gameManager = new GameManager(new HumanPlayer(), new ComputerPlayer());
+
+do
+{
+    RoundResult result = gameManager.PlayRound();
+
+    if (result == RoundResult.PlayerOneWins)
+    {
+        Console.WriteLine("Player 1 Wins!");
+    }
+    else if (result == RoundResult.PlayerTwoWins)
+    {
+        Console.WriteLine("Player 2 Wins!");
+    }
+    else { Console.WriteLine("Its a draw!"); }
+
+    Console.WriteLine("Play Again (Y/N)? ");
+
+} while (Console.ReadLine() == "Y");
